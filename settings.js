342c7c6d5a50b733b9ed795b448d29a3019d4e10
R4ptor44 <modiakha012@outlook.com>
2025-05-28 15:01:59 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Diakhaté', // Your name
-  ownerNumber: '769649261', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '221769649261', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
