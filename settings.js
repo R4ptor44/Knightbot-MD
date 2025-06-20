5e25980366421f73f3507c1cd05b0476e945755c
R4ptor44 <modiakha012@outlook.com>
2025-06-20 20:14:45 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Phase-2",
-  botOwner: 'Diakhaté', // Your name
+  botName: "𝐁𝐥𝐚𝐜𝐤-𝐁𝐨𝐭 𝐌𝐃",
+  botOwner: '𝐁𝐥𝐚𝐜𝐤', // Your name
   ownerNumber: '221769649261', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
